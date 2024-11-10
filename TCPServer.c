c924ddbbc0b626a41a1d5955528f1afdfd07f78f
Shpati <shpat.mjeku@student.uni-pr.edu>
2024-11-10 14:13:38 +0100

    Pranimi i lidhjeve me klienta tek serveri dhe shkembimi i mesazheve

--- TCPServer.c
+++ TCPServer.c
@@ -8,6 +8,33 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+
+
+// Funksioni që do të ekzekutohet nga çdo thread qe krijohet për menaxhimin e klientave
+void handle_client(int newsockfd, struct sockaddr_in cli_addr) {
+    char client_ip[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &(cli_addr.sin_addr), client_ip, INET_ADDRSTRLEN);
+    printf("Serveri u lidh me klientin %s\n", client_ip);
+
+    // Komunikimi dhe logimi i mesazheve
+    char buffer[256];
+    while (1) {
+        int bytes_received = recv(newsockfd, buffer, 255, 0);
+        if (bytes_received <= 0) {
+            break;
+        }
+
+        printf("Klienti %s: %s\n", client_ip, buffer);
+
+        // Dërgo një mesazh te klienti
+        char *server_message = "Ky është një mesazh nga serveri.";
+        send(newsockfd, server_message, strlen(server_message) + 1, 0);
+    }
+
+    close(newsockfd);
+    printf("Klienti %s disconnected.\n", client_ip);
+}
+
 int main() {
 
     // 1. Deklarimi i variablave për portin dhe IP adresën
@@ -34,6 +61,11 @@ int main() {
     // 6. Server loop
     while (1) {
         // 7. Pranimi lidhjes me klient
+        struct sockaddr_in cli_addr;
+        socklen_t clilen = sizeof(cli_addr);
+        int newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+
+        handle_client(newsockfd, cli_addr);
     }
 
     return 0;
